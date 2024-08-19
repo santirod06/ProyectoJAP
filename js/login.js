2ca203b5860ddd9745d5159a59ca3f6702d673b0
SantiRod <santi.rodriguez.2004.sr@gmail.com>
2024-08-19 15:04:49 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button_ingresar= document.getElementById("get_into");
+    button_ingresar.addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    const form= document.getElementById("form_login");
+    const user = document.getElementById('user').value.trim();
+    const password = document.getElementById('password').value.trim();
+    
+    document.getElementById("get_into").onclick = function() {
+    if (!user || !password) {
+        alert('Completar todos los campos'); //Modificar, en vez de alerta hacer un cartel
+    } else {
+            window.location.href = 'index.html';   
+    }
+}
+});
+});
